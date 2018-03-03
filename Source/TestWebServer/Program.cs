9a9c0137fb49771859f8ac96fcc6c425a38a5b75
ipponshimeji <ipponshimeji36@live.jp>
2018-03-03 10:22:09 +0900

    backup

---
+++ Source/TestWebServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace MAPE.Test.TestWebServer {
+    class Program {
+		public const int SuccessExitCode = 0;
+		public const int ErrorExitCode = 0;
+
+		static int Main(string[] args) {
+			// argument checks
+			string proxyPrefix = null;  // prefix as a proxy style web server
+			string directPrefix = null; // prefix as a direct style web server 
+			int argCount = args.Length;
+			if (0 < argCount) {
+				proxyPrefix = args[0];
+				if (1 < argCount) {
+					directPrefix = args[1];
+				}
+			}
+			if (proxyPrefix == null) {
+				ShowUsage();
+				return ErrorExitCode;
+			}
+
+			// run web server
+			int exitCode = SuccessExitCode;
+			Server server = new Server(proxyPrefix, directPrefix);
+			try {
+				// start the web server
+				server.Start();
+
+				// run until a line received from the standard input
+				Console.Out.WriteLine("Hit Enter key to quit.");
+				Console.In.ReadLine();
+			} catch (Exception exception) {
+				Console.Error.WriteLine(exception);
+				exitCode = ErrorExitCode;
+			} finally {
+				server.Stop();
+			}
+
+			return exitCode; 
+        }
+
+		private static void ShowUsage() {
+			TextWriter output = Console.Out;
+
+			output.WriteLine("USAGE:");
+			output.WriteLine("TestWebServer proxyPrefix [directPrefix]");
+		}
+	}
+}
---
+++ Source/TestWebServer/Server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+namespace MAPE.Test.TestWebServer {
+    class Server {
+		#region properties
+
+		protected string ProxyPrefix { get; }
+
+		protected string DirectPrefix { get; }
+
+		protected HttpListener Listener { get; private set; }
+
+		#endregion
+
+
+		#region creation and disposal
+
+		public Server(string proxyPrefix, string directPrefix) {
+			// argument checks
+			if (string.IsNullOrEmpty(proxyPrefix)) {
+				throw new ArgumentNullException(nameof(proxyPrefix));
+			}
+			// directPrefix can be null
+
+			// initialize members
+			this.ProxyPrefix = proxyPrefix;
+			this.DirectPrefix = directPrefix;
+
+			return;
+		}
+
+		#endregion
+
+
+		#region methods
+
+		public void Start() {
+			HttpListener listener = new HttpListener();
+			try {
+				// setup HttpListener
+				listener.Prefixes.Add(this.ProxyPrefix);
+				if (this.DirectPrefix != null) {
+					Listener.Prefixes.Add(this.DirectPrefix);
+				}
+
+				listener.Start();
+				this.Listener = listener;
+			} catch {
+			}
+		}
+
+		public void Stop() {
+			// state checks
+			Debug.Assert(this.Listener != null);
+
+			this.Listener.Stop();
+		}
+
+		public void Handle(HttpListenerContext context) {
+			HttpListenerRequest request = context.Request;
+			HttpListenerResponse response = context.Response;
+
+
+		}
+
+		#endregion
+	}
+}
